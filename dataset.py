31256f8a9a471e34ae947c7af56a6833217e031e
Dilara Gokay <dilaragokay@gmail.com>
2020-07-21 12:13:14 +0200

    refactor(dataset): Have seperate datasets for RGB and depth images

--- dataset.py
+++ dataset.py
@@ -3,11 +3,37 @@ import PIL.Image as pil
 import cv2
 import os.path
 
-data_root = "./data/val/"
-
 
 class KittiHumanDepthDataset(Dataset):
-    def __init__(self, scenes_filename):
+    def __init__(self, scenes_filename, data_root):
+        scenes = []
+        with open(scenes_filename) as f:
+            lines = f.read().splitlines()
+
+        for line in lines:
+            scenes.append(line.split('/'))
+
+        self.depth_files = []
+
+        for item in scenes:
+            depth_file = os.path.join(data_root, 'gt', item[0] + '_sync',
+                                      'proj_depth', 'groundtruth', 'image_02',
+                                      item[1])
+
+            if os.path.isfile(depth_file):
+                self.depth_files.append(depth_file)
+
+    def __len__(self):
+        return len(self.depth_files)
+
+    def __getitem__(self, idx):
+        depth = None
+        if self.depth_files:
+            depth = cv2.imread(self.depth_files[idx], -1) / 255.
+        return depth
+
+class KittiRGBDataset(Dataset):
+    def __init__(self, scenes_filename, data_root):
         scenes = []
         with open(scenes_filename) as f:
             lines = f.read().splitlines()
@@ -16,26 +42,21 @@ class KittiHumanDepthDataset(Dataset):
             scenes.append(line.split('/'))
 
         self.rgb_files = []
-        self.depth_files = []
 
         for item in scenes:
             rgb_file = os.path.join(data_root, 'raw', item[0] + '_sync',
                                     'image_02', 'data', item[1])
-            depth_file = os.path.join(data_root, 'gt', item[0] + '_sync',
-                                      'proj_depth', 'groundtruth', 'image_02',
-                                      item[1])
 
-            if os.path.isfile(rgb_file) and os.path.isfile(depth_file):
+            if os.path.isfile(rgb_file):
                 self.rgb_files.append(rgb_file)
-                self.depth_files.append(depth_file)
 
     def __len__(self):
         return len(self.rgb_files)
 
     def __getitem__(self, idx):
+        rgb_im = None
+        if self.rgb_files:
+            rgb_im = cv2.imread(self.rgb_files[idx])
+        return rgb_im
 
-        rgb_im = pil.open(self.rgb_files[idx])
 
-        depth = cv2.imread(self.depth_files[idx], -1) / 255.
-
-        return rgb_im, depth
