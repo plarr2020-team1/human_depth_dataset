6a67893228d0d9dcabe3548aef519e99fc65ff2d
Enis Simsar <enis.simsar@boun.edu.tr>
2020-07-22 20:50:32 +0300

    masks extractor

--- human_depth_dataset/dataset.py
+++ human_depth_dataset/dataset.py
@@ -1,12 +1,13 @@
-from torch.utils.data import Dataset
 import cv2
+import pickle
 import os.path
-import PIL.Image as pil
 import numpy as np
+import PIL.Image as pil
+from torch.utils.data import Dataset
 
 
 class KittiHumanDepthDataset(Dataset):
-    def __init__(self, scenes_filename, data_root='../data/kitti/val/'):
+    def __init__(self, scenes_filename, data_root='../data/kitti/val/', mask_file='../data/kitti/yolact.pkl'):
         scenes = []
         with open(scenes_filename) as f:
             lines = f.read().splitlines()
@@ -27,23 +28,27 @@ class KittiHumanDepthDataset(Dataset):
             if os.path.isfile(rgb_file) and os.path.isfile(depth_file):
                 self.rgb_files.append(rgb_file)
                 self.depth_files.append(depth_file)
+        
+        self.masks = None
+        if mask_file != None:
+            with open(mask_file, 'rb') as f:
+                self.masks = pickle.load(f)
 
     def __len__(self):
         return len(self.rgb_files)
 
     def __getitem__(self, idx):
-
         rgb_im = pil.open(self.rgb_files[idx])
 
         depth = cv2.imread(self.depth_files[idx], -1) / 255.
 
-        file = self.rgb_files[0].split('/')[-6] + '/' + self.rgb_files[0].split('/')[-1]
-
-        return {'rgb': rgb_im, 'depth': depth, 'index': file}
+        file = self.rgb_files[idx].split('/')[-4] + '/' + self.rgb_files[idx].split('/')[-1]
+        mask = self.masks[file] if self.masks != None else None
+        return {'rgb': rgb_im, 'depth': depth, 'index': file, 'mask': mask}
 
 
 class RGBDPeopleDataset(Dataset):
-    def __init__(self, data_root='../data/rgbd/'):
+    def __init__(self, data_root='../data/rgbd/', mask_file='../data/rgbd/yolact.pkl'):
         rgb_dir = 'rgb'
         depth_dir = 'depth'
 
@@ -56,6 +61,11 @@ class RGBDPeopleDataset(Dataset):
             filename = os.path.splitext(os.path.basename(path))[0]
             self.depth_files.append(
                 os.path.join(data_root, depth_dir, filename+'.pgm'))
+            
+        self.masks = None
+        if mask_file != None:
+            with open(mask_file, 'rb') as f:
+                self.masks = pickle.load(f)
 
     def __len__(self):
         return len(self.rgb_files)
@@ -70,5 +80,8 @@ class RGBDPeopleDataset(Dataset):
         # According to the dataset paper: http://www2.informatik.uni-freiburg.de/~spinello/spinelloIROS11.pdf
         depth = 8 * 0.075 * 594.2 / (1084 - depth)
         depth = np.rot90(depth)
+        
+        index = self.rgb_files[idx].split('/')[-1]
+        mask = self.masks[index] if self.masks != None else None
 
-        return {'rgb': rgb_im, 'depth': depth, 'index': self.rgb_files[idx].split('/')[-1]}
+        return {'rgb': rgb_im, 'depth': depth, 'index': index, 'mask': mask}
